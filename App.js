1580dfc9227c85a4bb10986b544c4c18de433a5f
sagarsam96 <92667993+sagarsam96@users.noreply.github.com>
2022-02-27 15:21:36 +0530

    upadate

--- App.js
+++ App.js
@@ -8,12 +8,24 @@ console.log(commentSection.style.display);
 const list = document.getElementById("list");
 
 
+const currDate = new Date();
+var currTime = currDate.getHours()%12 + ":" + currDate.getMinutes();
+
+
 inputComment.addEventListener("keypress", (e)=>{
     if (e.key === 'Enter'){
     // alert(inputComment.value);
+
     commentSection.style.display = "block";
+    
     const listElement = document.createElement("li");
-    listElement.innerText= inputComment.value;
+    listElement.innerText= inputComment.value + " ";
+    // const hr = document.createElement("hr");
+    // listElement.append(hr);
+    const time = document.createElement("span");
+    time.innerText= currTime;
+    listElement.appendChild(time);
+    
     list.appendChild(listElement);
 
     inputComment.value="";
